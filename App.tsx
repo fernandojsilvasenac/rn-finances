8327ed6d1c23a0ba128be5b05498a2f6ff1180a0
FERNANDO JACINTO DA SILVA <fernando.jsilva9@senacsp.edu.br>
2022-10-06 22:30:16 -0300

    my first commit, added theme colors, theme fonts

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/global/styles/theme';
+
+import { Dashboard } from './src/screens/Dashboard';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  })
+  if(!fontsLoaded) return null;
+  
+  SplashScreen.hideAsync();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard/>
+    </ThemeProvider>    
+  );
+}
+
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,28 @@
+export default {
+    colors: {
+        blue: '#5636D3',
+
+        orange: '#FF872C',
+        organge_light:'rgba(255,135,44,0.3)',
+
+        green: '#12A454',
+        green_light: 'rgba(18,164,84, 0.5)',
+
+        red: '#E83F5B',
+        red_light: 'rgba(232,63,91,0.5)',
+
+        shape: '#FFFFFF',
+
+        title: '#363F5F',
+        text: '#969CB2',
+        text_dark: '#000000',
+
+        background: '#F0F2F5',
+    },
+
+    fonts:{
+        regular: 'Poppins_400Regular',
+        medium: 'Poppins_500Medium',
+        bold: 'Poppins_700Bold'
+    }
+}
