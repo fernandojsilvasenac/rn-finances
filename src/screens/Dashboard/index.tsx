d633bb2e4ff42532938a4b89a1c6519f817aef5c
FERNANDO JACINTO DA SILVA <fernando.jsilva9@senacsp.edu.br>
2022-10-10 22:12:42 -0300

    added dashboard styled-components, added components HighlighCard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,44 @@
 import React from 'react'
+import { HighlightCard } from '../../components/HighlightCard'
+import { 
+  Container, 
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreething,
+  UserName,
+  UserWrapper,
+  Icon,
+  HighlightCards,
 
-import { Container, Title } from './styles';
+} from './styles';
 
 export function Dashboard() {
     return (
       <Container>
-        <Title>Dashboard...</Title>
+        <Header>
+          <UserWrapper>
+
+            <UserInfo>
+              <Photo 
+                source={ {uri:'https://avatars.githubusercontent.com/u/37640000?v=4'} }
+              />
+              <User>
+                <UserGreething>Olá,</UserGreething>
+                <UserName>Fernando</UserName>
+              </User>
+            </UserInfo>
+
+            <Icon name="power" />
+
+          </UserWrapper>
+        </Header>
+        <HighlightCards>
+          <HighlightCard />
+          <HighlightCard />
+          <HighlightCard />
+        </HighlightCards>
       </Container>
     )
 }
